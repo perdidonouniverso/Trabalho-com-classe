324c705cee920850d5c6bfaac6cb3433ae6e0d6b
bru4ganzala <110946783+bru4ganzala@users.noreply.github.com>
2022-09-13 19:48:58 -0300

    commit

---
+++ cadastroDoCliente.cs
@@ -0,0 +1,38 @@
+namespace POO{
+
+    public class CadstroDoCliente      {
+
+        private string cpf= "";
+        private string nome= "";
+        private string rg= "";
+        private string dataNacimento= "";
+
+        public string GetCpf(){
+            return cpf;
+        }
+        public void SetCpf(string cpf){
+            this.cpf = cpf;
+        }
+        public string GetNome(){
+            return nome;
+        }
+        public void SetNome(string nome){
+            this.nome = nome;
+        }
+        public string GetRg(){
+            return rg;
+        }
+        public void SetRg(string rg){
+            this.rg = rg;
+        }
+        public string GetDataNacimento(){
+            return dataNacimento;
+        }
+        public void SetDataNacimento(string dataNacimento){
+            this.dataNacimento = dataNacimento;
+#if true
+	    }
+	
+#endif   
+     }
+}
